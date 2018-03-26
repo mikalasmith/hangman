e9c5832b9ed68c29db792908c79b9208cb5c6851
Mikala Smith <mikalasmith@mikalas-air.home>
2018-03-26 01:20:37 -0400

    updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// Create an array of words 
+var word =["moonlight", "titanic", "crash", 
+"gravity", "avatar", "starwars"];
+
+// Choose words randomly
+var randNum = Math.floor(Math.random() * word.length);
+var chosenWord = word[randNum];
+// Create underscores based on length of word
+
+// Get users guess
+// Check if guess is right
+// if right, push to right array
+// if wrong, push to wrong array
